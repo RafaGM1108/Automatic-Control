f887a874d5e79751c4ffca856cea3c1a7936850b
Rafael <rafael11082003@gmail.com>
2023-11-01 12:08:44 -0500

    *

--- Laboratorio4/bono.py
+++ Laboratorio4/bono.py
@@ -35,46 +35,34 @@ if uploaded_file is not None:
         ax2.set_ylabel('Fase (grados)')
         ax2.grid()
 
-
-        # Calculate and display cutoff frequency and bandwidth
-        
-        
         A = None
         ancho_de_banda = None
         margen_de_fase = None
         margen_de_ganancia = None
         pico = None
         frecuencia_de_resonancia = None
-
-        # Encuentra el índice del pico (máxima magnitud)
-        
-        
+                
         cutoff_indices = np.where(magnitud_dB <= 0.707)[0]
         if len(cutoff_indices) > 0:
             first_index = cutoff_indices[0]
             A = frecuencia[first_index]
 
-            # Encuentra el último índice que cumple la condición
             last_index = cutoff_indices[-1]
 
-            # Calcula el ancho de banda
             ancho_de_banda = frecuencia[last_index] - frecuencia[first_index]
 
         st.write("Frecuencia de corte:", A)
         st.write("Ancho de Banda:", ancho_de_banda)
 
-
-        # Encuentra el índice de la frecuencia de cruce donde la magnitud es 0 dB
         crossing_indices = np.where(np.diff(np.signbit(magnitud_dB)))[0]
         if len(crossing_indices) > 0:
-            # Calcula el margen de ganancia
+    
             gain_margin_index = crossing_indices[0]
             margen_de_ganancia = -magnitud_dB[gain_margin_index]
 
-        # Encuentra el índice de la frecuencia de cruce donde la fase es -180 grados
         phase_crossing_indices = np.where(np.diff(np.signbit(fase_grados + 180)))[0]
         if len(phase_crossing_indices) > 0:
-            # Calcula el margen de fase
+            
             phase_margin_index = phase_crossing_indices[0]
             margen_de_fase = -180 - fase_grados[phase_margin_index]
 
@@ -83,7 +71,7 @@ if uploaded_file is not None:
         
         peak_magnitude_index = np.argmax(magnitud_dB)
         if peak_magnitude_index is not None:
-            # Calcula el pico y la frecuencia de resonancia
+            
             pico = magnitud_dB[peak_magnitude_index]
             frecuencia_de_resonancia = frecuencia[peak_magnitude_index]
 
